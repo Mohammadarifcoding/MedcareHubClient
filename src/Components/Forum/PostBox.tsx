25ef41636259b80fc8d2f354e3aa14afc8da6ef6
iamapusaha <apusaha392@gmail.com>
2024-01-26 20:22:41 +0600

    added post section

---
+++ src/Components/Forum/PostBox.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+interface PostData {
+    id: number;
+    name: string;
+    img: string;
+    tag: string;
+    comment: number;
+    date: string;
+    heading: string;
+    post_body: string;
+}
+
+
+const PostBox = () => {
+    const [post, setPost] = useState<PostData[]>([]);
+    useEffect(() => {
+        fetch('post.json')
+            .then(res => res.json())
+            .then(data => setPost(data))
+    }, [])
+    return (
+        <div>
+            {
+                post.map(data => <SinglePost key={data.id} data={data}></SinglePost>)
+            }
+        </div>
+    );
+};
+
+export default PostBox;
