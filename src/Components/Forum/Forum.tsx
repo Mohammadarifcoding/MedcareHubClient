c6eb7d3946cfec310c7de7645323d24d92f9fdc6
iamapusaha <apusaha392@gmail.com>
2024-01-26 20:17:39 +0600

    added create post section on forum

--- src/Components/Forum/Forum.tsx
+++ src/Components/Forum/Forum.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import FNavBar from './FNavBar.tsx';
+import CreatePost from './CreatePost.tsx';
 
 const Forum = () => {
     return (
         <div className='container mx-auto'>
 
             <FNavBar></FNavBar>
+            <CreatePost></CreatePost>
         </div>
     );
 };
---
+++ src/Components/Forum/PostMenu.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostMenu = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default PostMenu;
