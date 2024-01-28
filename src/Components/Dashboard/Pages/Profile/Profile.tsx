22cbe3e04af8e7515ee5318badf2afebf9109a1d
Mouri199 <138799900+Mouri199@users.noreply.github.com>
2024-01-28 20:37:26 +0600

    user profile design updated more

--- src/Components/Dashboard/Pages/Profile/Profile.tsx
+++ src/Components/Dashboard/Pages/Profile/Profile.tsx
@@ -11,141 +11,178 @@ const Profile = () => {
                 'Unknown';
 
     return (
-        <div>
-
+        <>
             {
-                status === 'Unknown' && (
-                    <div className="mt-20 flex flex-col items-center justify-center text-[#0360D9]">
+                status === "Unknown" &&
+                (
+                    <div className="divide-y max-w-2xl mx-auto divide-gray-200">
+                        <div className="py-4">
+                            <h2 className="mb-2 lg:text-3xl text-lg my-5 text-center font-semibold">Profile Information</h2>
+                            <div className="flex items-center justify-between">
+                                <div className="flex items-center space-x-3">
+                                    <span className="relative mt-5 flex h-48 w-48 shrink-0 overflow-hidden rounded-full">
+                                        <img
+                                            className="aspect-square h-full w-full"
+                                            alt=""
+                                            src={user?.photoURL}
+                                        />
+                                    </span>
 
-                        <img className='w-[230px] mb-5 border-[3px] border-[#0360D9] rounded-full' src={user?.photoURL} alt="" />
-
-                        <h1 className='mb-5 text-xl font-semibold'>Status: {status} </h1>
-
-                        <div className="flex md:flex-row md:items-center md:space-x-6 space-y-6 md:space-y-0 bg-[#A5CCFF] p-6">
-
-
-                            <div className="flex md:flex-row md:items-center md:space-x-12">
-                                <div className="flex flex-col space-y-2">
-                                    <span>Your Name</span>
-                                    <input
-                                        className="flex h-10 w-full rounded-md border border-input px-3 py-2 text-sm  text-[#0360D9] placeholder-gray-500"
-                                        readOnly defaultValue={user?.displayName}
-                                    />
                                 </div>
-                                <div className="flex flex-col space-y-2">
-                                    <span>Your Email</span>
-                                    <input
-                                        className="flex h-10 w-full rounded-md border border-input px-3 py-2 text-sm  text-[#0360D9] placeholder-gray-500"
-                                        readOnly defaultValue={user?.email}
-                                        type="email"
-                                    />
-                                </div>
+                            </div>
+                        </div>
+                        <div className="py-4">
+                            <h2 className="mb-2 text-lg font-semibold">Status</h2>
+                            <div className="flex justify-between">
+                                <span>{status}</span>
 
                             </div>
-
                         </div>
-
-                        <div className="flex mt-5 md:flex-row md:items-center md:space-x-6 space-y-6 md:space-y-0 bg-[#A5CCFF] p-6">
-
-
-                            <div className="flex md:flex-row md:items-center md:space-x-12">
-                                <div className="flex flex-col space-y-2">
-                                    <span>Phone Number</span>
-                                    <input
-                                        className="flex h-10 w-full rounded-md border border-input px-3 py-2 text-sm  text-[#0360D9] placeholder-gray-500"
-                                        readOnly defaultValue={user?.phone}
-                                    />
-                                </div>
-                                <div className="flex flex-col space-y-2">
-                                    <span>Address</span>
-                                    <input
-                                        className="flex h-10 w-full rounded-md border border-input px-3 py-2 text-sm  text-[#0360D9] placeholder-gray-500"
-                                        readOnly defaultValue={user?.address}
-                                        type="email"
-                                    />
-                                </div>
+                        <div className="py-4">
+                            <h2 className="mb-2 text-lg font-semibold">Username</h2>
+                            <div className="flex justify-between">
+                                <span>{user?.displayName}</span>
 
                             </div>
-
                         </div>
+                        
+                        <div className="py-4">
+                            <h2 className="mb-2 text-lg font-semibold">Email Address</h2>
+                            <div className="space-y-2">
+                                <div className="flex items-center justify-between">
+                                    <div>
+                                        <span>{user?.email}</span>
+                                    </div>
 
+                                </div>
+
+                                <button className="inline-flex items-center justify-center  rounded-md text-sm font-medium hover:bg-[#0360D9] hover:text-white h-10 px-4 py-2 text-blue-600">
+                                    Update Profile
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 )
             }
             {
-                status === 'Doctor' && (
-                    <div className="mt-20 flex flex-col items-center justify-center text-[#0360D9]">
+                status === "Company User" &&
+                (
+                    <div className="divide-y max-w-2xl mx-auto divide-gray-200">
+                        <div className="py-4">
+                            <h2 className="mb-2 lg:text-3xl text-lg my-5 text-center font-semibold">Profile Information</h2>
+                            <div className="flex items-center justify-between">
+                                <div className="flex items-center space-x-3">
+                                    <span className="relative mt-5 flex h-48 w-48 shrink-0 overflow-hidden rounded-full">
+                                        <img
+                                            className="aspect-square h-full w-full"
+                                            alt=""
+                                            src={user?.photoURL}
+                                        />
+                                    </span>
 
-                        <img className='w-[230px] mb-5 border-[3px] border-[#0360D9] rounded-full' src={user?.photoURL} alt="" />
-
-                        <h1 className='mb-5 text-xl font-semibold'>Status: {status} </h1>
-
-                        <div className="flex md:flex-row md:items-center md:space-x-6 space-y-6 md:space-y-0 bg-[#A5CCFF] p-6">
-
-
-                            <div className="flex md:flex-row md:items-center md:space-x-12">
-                                <div className="flex flex-col space-y-2">
-                                    <span>Your Name</span>
-                                    <input
-                                        className="flex h-10 w-full rounded-md border border-input px-3 py-2 text-sm  text-[#0360D9] placeholder-gray-500"
-                                        readOnly defaultValue={user?.displayName}
-                                    />
                                 </div>
-                                <div className="flex flex-col space-y-2">
-                                    <span>Your Email</span>
-                                    <input
-                                        className="flex h-10 w-full rounded-md border border-input px-3 py-2 text-sm  text-[#0360D9] placeholder-gray-500"
-                                        readOnly defaultValue={user?.email}
-                                        type="email"
-                                    />
-                                </div>
+                            </div>
+                        </div>
+                        <div className="py-4">
+                            <h2 className="mb-2 text-lg font-semibold">Status</h2>
+                            <div className="flex justify-between">
+                                <span>{status}</span>
 
                             </div>
-
                         </div>
+                        <div className="py-4">
+                            <h2 className="mb-2 text-lg font-semibold">Username</h2>
+                            <div className="flex justify-between">
+                                <span>{user?.displayName}</span>
 
+                            </div>
+                        </div>
+                        <div className="py-4">
+                            <h2 className="mb-2 text-lg font-semibold">Email address</h2>
+                            <div className="space-y-2">
+                                <div className="flex items-center justify-between">
+                                    <div>
+                                        <span>{user?.email}</span>
+                                    </div>
+
+                                </div>
+
+                                <button className="inline-flex items-center justify-center  rounded-md text-sm font-medium hover:bg-[#0360D9] hover:text-white h-10 px-4 py-2 text-blue-600">
+                                    Update Profile
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 )
             }
             {
-                status === 'Company User' && (
-                    <div className="mt-20 flex flex-col items-center justify-center text-[#0360D9]">
+                status === "Doctor" &&
+                (
+                    <div className="divide-y max-w-2xl mx-auto divide-gray-200">
+                        <div className="py-4">
+                            <h2 className="mb-2 lg:text-3xl text-lg my-5 text-center font-semibold">Profile Information</h2>
+                            <div className="flex items-center justify-between">
+                                <div className="flex items-center space-x-3">
+                                    <span className="relative mt-5 flex h-48 w-48 shrink-0 overflow-hidden rounded-full">
+                                        <img
+                                            className="aspect-square h-full w-full"
+                                            alt=""
+                                            src={user?.photoURL}
+                                        />
+                                    </span>
 
-                        <img className='w-[230px] mb-5 border-[3px] border-[#0360D9] rounded-full' src={user?.photoURL} alt="" />
-
-                        <h1 className='mb-5 text-xl font-semibold'>Status: {status} </h1>
-
-                        <div className="flex md:flex-row md:items-center md:space-x-6 space-y-6 md:space-y-0 bg-[#A5CCFF] p-6">
-
-
-                            <div className="flex md:flex-row md:items-center md:space-x-12">
-                                <div className="flex flex-col space-y-2">
-                                    <span>Company Name</span>
-                                    <input
-                                        className="flex h-10 w-full rounded-md border border-input px-3 py-2 text-sm  text-[#0360D9] placeholder-gray-500"
-                                        readOnly defaultValue={user?.displayName}
-                                    />
                                 </div>
-                                <div className="flex flex-col space-y-2">
-                                    <span>Company Email</span>
-                                    <input
-                                        className="flex h-10 w-full rounded-md border border-input px-3 py-2 text-sm  text-[#0360D9] placeholder-gray-500"
-                                        readOnly defaultValue={user?.email}
-                                        type="email"
-                                    />
-                                </div>
+                            </div>
+                        </div>
+                        <div className="py-4">
+                            <h2 className="mb-2 text-lg font-semibold">Status</h2>
+                            <div className="flex justify-between">
+                                <span>{status}</span>
 
                             </div>
+                        </div>
+                        <div className="py-4">
+                            <h2 className="mb-2 text-lg font-semibold">Username</h2>
+                            <div className="flex justify-between">
+                                <span>{user?.displayName}</span>
 
+                            </div>
                         </div>
 
+                        <div className="py-4">
+                            <h2 className="mb-2 text-lg font-semibold">Email Address</h2>
+                            <div className="flex justify-between">
+                                <span>{user?.email}</span>
 
+                            </div>
+                        </div>
+                        <div className="py-4">
+                            <h2 className="mb-2 text-lg font-semibold">Phone No</h2>
+                            <div className="flex justify-between">
+                                <span>{user?.phoneno}</span>
+
+                            </div>
+                        </div>
+                        <div className="py-4">
+                            <h2 className="mb-2 text-lg font-semibold">Address</h2>
+                            <div className="space-y-2">
+                                <div className="flex items-center justify-between">
+                                    <div>
+                                        <span>{user?.address}</span>
+                                    </div>
+
+                                </div>
+
+                                <button className="inline-flex items-center justify-center  rounded-md text-sm font-medium hover:bg-[#0360D9] hover:text-white h-10 px-4 py-2 text-blue-600">
+                                    Update Profile
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 )
             }
 
-
-        </div>
+        </>
     );
 };
 
