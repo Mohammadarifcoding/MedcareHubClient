dfe68d21d55d1e9eec23709f76dfdc998c30a38d
iamapusaha <apusaha392@gmail.com>
2024-01-31 19:31:41 +0600

    added modal form

--- src/Components/Forum/CreatePost.tsx
+++ src/Components/Forum/CreatePost.tsx
@@ -3,28 +3,54 @@ import { IoMdPhotos } from "react-icons/io";
 import UseAuth from '../../Hook/UseAuth.tsx';
 
 const CreatePost = () => {
-    const {user} = UseAuth()
+    const { user } = UseAuth()
     return (
-        <div className="px-11 py-5 mx-auto bg-slate-200">
-            <div className="flex gap-5">
-                <div>
-                    <img className="w-[50px] h-[50px] bg-slate-500 object-cover rounded-lg hover:blur-[2px] duration-500" src={user?.photoURL} alt="" />
-                </div>
-                <div className="w-full">
+        <>
+            <div className="px-11 py-5 mx-auto bg-slate-200">
+                <div className="flex gap-5">
+                    <div>
+                        <img className="w-[50px] h-[50px] bg-slate-500 object-cover rounded-lg hover:blur-[2px] duration-500" src={user?.photoURL} alt="" />
+                    </div>
+                    <div className="w-full">
 
-                    <input id="u_email" type="u_email" placeholder="Share or Ask Somethings to Everyone" className="p-3  w-full outline-none border rounded-md invalid:border-red-700 valid:border-black" />
+                        <input id="u_email" type="u_email" placeholder="Share or Ask Somethings to Everyone" className="p-3  w-full outline-none border rounded-md invalid:border-red-700 valid:border-black" />
+                    </div>
+                </div>
+                <div className="flex justify-between pt-5">
+                    <div className="flex items-center gap-2">
+                        <IoMdPhotos />
+                        <h1>photo/video</h1>
+                    </div>
+
+
+                    <label htmlFor="my_modal_7" className="btn">open modal</label>
                 </div>
             </div>
-            <div className="flex justify-between pt-5">
-                <div className="flex items-center gap-2">
-                    <IoMdPhotos />
-                    <h1>photo/video</h1>
-                </div>
+            <div>
 
-                <button className="bg-blue-300 p-3 rounded">Add post</button>
+
+                {/* Put this part before </body> tag */}
+                <input type="checkbox" id="my_modal_7" className="modal-toggle" />
+                <div className="modal" role="dialog">
+                    <div className="modal-box">
+                        <label className="form-control w-full">
+                            <h1>Create post</h1>
+
+                            <div className="label">
+                                <span className="label-text">What is your name?</span>
+                            </div>
+                            <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+
+                        </label>
+                    </div>
+                    <label className="modal-backdrop" htmlFor="my_modal_7">Close</label>
+                </div>
             </div>
-        </div>
+        </>
     );
+
+
+
 };
 
 export default CreatePost;
