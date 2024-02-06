edadf680928c4e0b7980bd5260e31be81d2d20da
iamapusaha <apusaha392@gmail.com>
2024-02-06 15:43:50 +0600

    showing post data by user need via category menu

--- src/Components/Forum/PostBox.tsx
+++ src/Components/Forum/PostBox.tsx
@@ -1,48 +1,60 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import SinglePost from './SinglePost.tsx';
 import UseAxiosPublic from '../../Hook/UseAxiosPublic.tsx';
 import { useQuery } from "@tanstack/react-query";
-import { FaSearch } from "react-icons/fa";
-interface PostData {
-    id: number;
-    name: string;
-    img: string;
-    tag: string;
-    comment: number;
-    date: string;
-    heading: string;
-    discription: string;
-}
+import UseAuth from '../../Hook/UseAuth.tsx';
+// interface PostData {
+//     id: number;
+//     name: string;
+//     img: string;
+//     tag: string;
+//     comment: number;
+//     date: string;
+//     heading: string;
+//     discription: string;
+// }
 
 
 const PostBox = () => {
+    const { user } = UseAuth()
+    const userEmail = user?.email;
     const axiosPublic = UseAxiosPublic();
-    const { data: post, refetch } = useQuery({
-        queryKey: ['post'],
+    const [queryType, setQueryType] = useState({ type: 'category', value: '' });
+
+    const { data: posts } = useQuery({
+        queryKey: [queryType.type, queryType.value],
         queryFn: async () => {
-            const res = await axiosPublic.get('/forum');
-            // console.log(res.data);
+            let res;
+            if (queryType.type === 'category') {
+                res = await axiosPublic.get(`/forum/${queryType.value}`);
+            } else if (queryType.type === 'userPost') {
+                res = await axiosPublic.get(`/api/forum/${queryType.value}`);
+            }
             return res?.data
         }
     })
+
+
     return (
         <div>
             <div className="px-11 py-5 mx-auto">
                 <div className="flex justify-center items-center">
                     <div>
                         <ul className="flex gap-2">
-                            <button className="border-4 p-2 border-s-violet-200">All Post</button>
-                            <button className="border-4 p-2 border-s-violet-200">My Post</button>
-                            <button className="border-4 p-2 border-s-violet-200">Dr. Post</button>
-                            <button className="border-4 p-2 border-s-violet-200">Patient Post</button>
+                            <button onClick={() => setQueryType({ type: 'category', value: '' })} className="border-4 p-2 border-s-violet-200">All Post</button>
+                            <button onClick={() => setQueryType({ type: 'userPost', value: userEmail })} className="border-4 p-2 border-s-violet-200">My Post</button>
+                            <button onClick={() => setQueryType({ type: 'category', value: 'Dr-Post' })} className="border-4 p-2 border-s-violet-200">Dr. Post</button>
+                            <button onClick={() => setQueryType({ type: 'category', value: 'patient-post' })} className="border-4 p-2 border-s-violet-200">Patient Post</button>
                         </ul>
                     </div>
+
                 </div>
             </div>
             <div>
                 {
-                    post?.map(data => <SinglePost key={data._id} data={data}></SinglePost>)
+                    posts?.map(data => <SinglePost key={data._id} data={data}></SinglePost>)
                 }
+
             </div>
         </div>
     );
