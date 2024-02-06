f902e404d0aa2135528c461d7e01e1b24266e313
afsana-jannat <afsanajannat205@gmail.com>
2024-02-06 16:19:20 +0600

    create doctor status page

--- src/Components/Dashboard/Layout/DashboardLayout.tsx
+++ src/Components/Dashboard/Layout/DashboardLayout.tsx
@@ -4,13 +4,8 @@ import { FaArrowRight, FaHome, FaUserCircle, FaUserFriends } from "react-icons/f
 import { MdOutlineNotificationsActive } from "react-icons/md";
 import { FaArrowLeft } from "react-icons/fa";
 import UseAuth from '../../../Hook/UseAuth.tsx';
-<<<<<<< HEAD
 import Drawer from 'react-modern-drawer'
 import { FaFilePrescription } from "react-icons/fa";
-
-=======
-import Drawer from 'react-modern-drawer';
->>>>>>> 5a7a16edd2fc3914bb1445a9ba2653d60be89ab0
 import { PiDotsNineBold } from "react-icons/pi";
 
 const DashboardLayout = () => {
@@ -34,28 +29,7 @@ const DashboardLayout = () => {
                 </div>
                 <div className='p-4 flex gap-2 justify-center my-auto flex-col xl:text-lg 2xl:text-xl'>
 
-<<<<<<< HEAD
-                        <NavLink to='/' className=' flex gap-1 items-center    hover:scale-105 overflow-hidden hover:bg-[#2c7feb] p-2 rounded-2xl transition-all duration-200'>
-                            <FaHome className=''></FaHome>  Home
-                        </NavLink>
-                        <NavLink to='/dashboard/profile' className=' flex gap-1 items-center    hover:scale-105 overflow-hidden hover:bg-[#2c7feb] p-2 rounded-2xl transition-all duration-200'>
-                            <FaUserCircle />  Profile
-                        </NavLink>
-                        <NavLink to='/dashboard/docstatus' className=' flex gap-1 items-center   
-                         hover:scale-105 overflow-hidden hover:bg-[#2c7feb] p-2 rounded-2xl transition-all duration-200'>
-                            <FaFilePrescription />  Doctor status
-                        </NavLink>
 
-
-                    </> : <>
-                        <NavLink to='/' className=' flex justify-center   hover:scale-105 overflow-hidden hover:bg-[#2c7feb] px-2 py-3 rounded-2xl transition-all duration-200'>
-                            <FaHome ></FaHome>
-                        </NavLink>
-                        <NavLink to='/dashboard/profile' className=' flex  justify-center   hover:scale-105 overflow-hidden hover:bg-[#2c7feb] px-2 py-3 rounded-2xl transition-all duration-200'>
-                            <FaUserCircle />
-                        </NavLink>
-                    </>}
-=======
                     {openLayout ? (
                         <>
                             <NavLink to='/' className=' flex gap-1 items-center hover:scale-105 overflow-hidden hover:bg-[#2c7feb] p-2 rounded-2xl transition-all duration-200'>
@@ -66,6 +40,9 @@ const DashboardLayout = () => {
                             </NavLink>
                             <NavLink to='/dashboard/comproduct' className=' flex gap-1 items-center hover:scale-105 overflow-hidden hover:bg-[#2c7feb] p-2 rounded-2xl transition-all duration-200'>
                                 <FaUserCircle /> Company Product
+                            </NavLink>
+                            <NavLink to='/dashboard/docstatus' className=' flex gap-1 items-center hover:scale-105 overflow-hidden hover:bg-[#2c7feb] p-2 rounded-2xl transition-all duration-200'>
+                                <FaUserCircle /> Doctor Status
                             </NavLink>
                             <NavLink to='/dashboard/alluser' className=' flex gap-1 items-center hover:scale-105 overflow-hidden hover:bg-[#2c7feb] p-2 rounded-2xl transition-all duration-200'>
                                 <FaUserFriends />  All User
@@ -82,12 +59,15 @@ const DashboardLayout = () => {
                             <NavLink to='/dashboard/comproduct' className=' flex justify-center hover:scale-105 overflow-hidden hover:bg-[#2c7feb] px-2 py-3 rounded-2xl transition-all duration-200'>
                                 <FaUserCircle />
                             </NavLink>
+                            <NavLink to='/dashboard/docstatus' className=' flex justify-center hover:scale-105 overflow-hidden hover:bg-[#2c7feb] px-2 py-3 rounded-2xl transition-all duration-200'>
+                                <FaUserCircle />
+                            </NavLink>
                             <NavLink to='/dashboard/alluser' className=' flex justify-center hover:scale-105 overflow-hidden hover:bg-[#2c7feb] px-2 py-3 rounded-2xl transition-all duration-200'>
                                 <FaUserFriends />
                             </NavLink>
                         </>
                     )}
->>>>>>> 5a7a16edd2fc3914bb1445a9ba2653d60be89ab0
+
                     <div onClick={() => { setOpenLayout(!openLayout) }} className={`absolute top-[50%] p-2 border rounded-full hover:scale-110 cursor-pointer transition-all duration-200 hover:right-3 right-1 `}>
                         {openLayout ? <FaArrowLeft /> : <FaArrowRight></FaArrowRight>}
                     </div>
@@ -112,29 +92,31 @@ const DashboardLayout = () => {
                                     <p><MdOutlineNotificationsActive></MdOutlineNotificationsActive></p>
                                 </div>
                                 <hr className='my-5 h-[2px]' />
-                                <div className='flex items-center gap-6 text-xl font-semibold text-[#0360D9]'>
+                                <div className='flex items-center gap-6 text-lg font-semibold text-[#0360D9]'>
                                     <p> <FaHome /></p>
                                     <NavLink to='/'>Home</NavLink>
                                 </div>
-                                <div className='flex items-center gap-6 text-xl font-semibold my-2 text-[#0360D9]'>
+                                <div className='flex items-center gap-6 text-lg font-semibold my-2 text-[#0360D9]'>
                                     <p><FaUserCircle /></p>
                                     <NavLink to='/dashboard/profile'>Profile</NavLink>
                                 </div>
-                                <div className='flex items-center gap-6 text-xl font-semibold my-2 text-[#0360D9]'>
-<<<<<<< HEAD
-                                <p><FaFilePrescription /></p>
+                                <div className='flex items-center gap-6 text-lg font-semibold my-2 text-[#0360D9]'>
+
+                                    <p><FaFilePrescription /></p>
                                     <NavLink to='/dashboard/docstatus'>Doctor Status</NavLink>
                                 </div>
-
-=======
+                                <div className='flex items-center gap-6 text-lg font-semibold my-2 text-[#0360D9]'>
                                     <p><FaUserFriends /> </p>
-                                    <p><FaUserFriends/> </p>
                                     <NavLink to='/dashboard/alluser'>All User</NavLink>
+                                </div>
+                                <div className='flex items-center gap-6 text-lg font-semibold my-2 text-[#0360D9]' >
                                     <p><FaUserCircle /></p>
                                     <NavLink to='/dashboard/comproduct'>Company Product</NavLink>
                                 </div>
->>>>>>> 5a7a16edd2fc3914bb1445a9ba2653d60be89ab0
+
                             </div>
+
+
                         </Drawer>
                         <h1 className='text-xl font-semibold text-[#0360D9]'>Dashboard</h1>
                     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -23,16 +23,13 @@ import DoctorDetails from './Components/Pages/Doctors/DoctorCard/DoctorDetails.t
 import Forum from './Components/Forum/Forum.tsx';
 import About from './Components/Pages/About/About.tsx';
 import CompanyDetails from './Components/Pages/CompanyDetails/CompanyDetails.tsx';
-<<<<<<< HEAD
 import DocStatus from './Components/Dashboard/Pages/DocStatus/DocStatus.tsx';
-=======
 import CompanyProduct from './Components/Dashboard/companyProduct/CompanyProduct.tsx';
 import DetailsMedicien from '../public/Asset/DetailsOfMedicine/DetailsMedicien.tsx';
 import MedicienDetails from './Components/Pages/Detailsofmediciens/MedicienDetails.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AllUser from './Components/Dashboard/Pages/AllUser.tsx';
 import CreateDoctor from './Components/Pages/CreateDoctors/CreateDoctor.jsx';
->>>>>>> 5a7a16edd2fc3914bb1445a9ba2653d60be89ab0
 
 const queryClient = new QueryClient()
 
@@ -81,12 +78,12 @@ const router = createBrowserRouter([
       {
         path: '/company/:companyname',
         element: <CompanyDetails></CompanyDetails>
-      },{
-        path:'/detailsmedicines',
-        element:<MedicienDetails></MedicienDetails>
-      },{
-        path:'/addoctor',
-        element:<CreateDoctor></CreateDoctor>
+      }, {
+        path: '/detailsmedicines',
+        element: <MedicienDetails></MedicienDetails>
+      }, {
+        path: '/addoctor',
+        element: <CreateDoctor></CreateDoctor>
       }
     ]
   },
@@ -97,20 +94,17 @@ const router = createBrowserRouter([
       {
         path: '/dashboard/profile',
         element: <Profile></Profile>
-<<<<<<< HEAD
       },
       {
-        path:'/dashboard/docstatus',
-       element: <DocStatus></DocStatus>
-=======
-      },{
-        path:'/dashboard/comproduct',
-        element:<CompanyProduct></CompanyProduct>
+        path: '/dashboard/docstatus',
+        element: <DocStatus></DocStatus>
+      }, {
+        path: '/dashboard/comproduct',
+        element: <CompanyProduct></CompanyProduct>
       },
       {
         path: '/dashboard/alluser',
         element: <AllUser></AllUser>
->>>>>>> 5a7a16edd2fc3914bb1445a9ba2653d60be89ab0
       }
     ]
   },
