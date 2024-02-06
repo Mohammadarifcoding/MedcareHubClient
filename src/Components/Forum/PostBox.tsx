1ad3d02cba89bf482be3daf8af96d7e0fa38de14
iamapusaha <apusaha392@gmail.com>
2024-02-06 21:07:22 +0600

    changed post category name

--- src/Components/Forum/PostBox.tsx
+++ src/Components/Forum/PostBox.tsx
@@ -38,7 +38,7 @@ const PostBox = () => {
                         <ul className="flex gap-2">
                             <button onClick={() => handleButtonClick('category', '')} className="border-4 p-2 border-s-violet-200">All Post</button>
                             <button onClick={() => handleButtonClick('userPost', userEmail)} className="border-4 p-2 border-s-violet-200">My Post</button>
-                            <button onClick={() => handleButtonClick('category', 'Dr-Post')} className="border-4 p-2 border-s-violet-200">Dr. Post</button>
+                            <button onClick={() => handleButtonClick('category', 'dr-post')} className="border-4 p-2 border-s-violet-200">Dr. Post</button>
                             <button onClick={() => handleButtonClick('category', 'patient-post')} className="border-4 p-2 border-s-violet-200">Patient Post</button>
                         </ul>
                     </div>
