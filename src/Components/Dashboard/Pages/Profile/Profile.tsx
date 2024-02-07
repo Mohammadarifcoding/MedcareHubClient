b68dca092677098a30fa272e4a21c4748b7201a8
Jihad Hasan <mdjihat0021@gmail.com>
2024-02-07 23:03:54 +0600

    finished update profile image implement

--- src/Components/Dashboard/Pages/Profile/Profile.tsx
+++ src/Components/Dashboard/Pages/Profile/Profile.tsx
@@ -6,12 +6,19 @@ import Swal from 'sweetalert2';
 import { base_URL } from '../../../../utills/BaseURL.ts';
  
  
+const image_hosting_key = '140f2d0db1502e65c2c0ee7bfc66be98';
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
  
 const Profile = () => {
     const { user } = UseAuth()
     const [userData, setUserData] = useState([]);
     const [openModal, setOpenModal] = useState(false);
     const [refetchData, setRefecthData] = useState(false)
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const handleFileChange = (event) => {
+        setSelectedFile(event.target.files[0]);
+    };
  
  
     const {
@@ -32,6 +39,28 @@ const Profile = () => {
  
     const handleUpdate = async (data) => {
         console.log(data);
+
+        
+        let imageUrl = data.photoURL; 
+
+        
+        if (selectedFile) {
+            const formData = new FormData();
+            formData.append('image', selectedFile);
+
+            try {
+                const response = await axios.post(image_hosting_api, formData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                });
+                imageUrl = response.data.data.url; 
+            } catch (error) {
+                console.error('Error uploading image:', error);
+                return;
+            }
+        }
+
         const formData = {
             name: data.name,
             email: data.email,
@@ -39,9 +68,9 @@ const Profile = () => {
             age: data.age,
             address: data.address,
             gender: data.gender,
-            imageURL: data.photoURL
+            imageURL: imageUrl
         }
-        console.log(data.id);
+        console.log(formData);
  
         await axios.put(`${base_URL}/User/${data.id}`, formData)
             .then((res) => {
@@ -57,7 +86,7 @@ const Profile = () => {
  
     }
  
-    console.log(userData.data);
+    // console.log(userData.data);
     return (
         <>
  
@@ -228,7 +257,29 @@ const Profile = () => {
  
  
                                         </div>
- 
+                                        <div className="py-3 justify-center flex">
+                        <label htmlFor="file" className="custum-file-upload">
+                            <div className="icon">
+                                <svg viewBox="0 0 24 24" fill xmlns="http://www.w3.org/2000/svg">
+                                    <g id="SVGRepo_bgCarrier" strokeWidth={0} />
+                                    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" />
+                                    <g id="SVGRepo_iconCarrier">
+                                        {' '}
+                                        <path
+                                            fillRule="evenodd"
+                                            clipRule="evenodd"
+                                            d="M10 1C9.73478 1 9.48043 1.10536 9.29289 1.29289L3.29289 7.29289C3.10536 7.48043 3 7.73478 3 8V20C3 21.6569 4.34315 23 6 23H7C7.55228 23 8 22.5523 8 22C8 21.4477 7.55228 21 7 21H6C5.44772 21 5 20.5523 5 20V9H10C10.5523 9 11 8.55228 11 8V3H18C18.5523 3 19 3.44772 19 4V9C19 9.55228 19.4477 10 20 10C20.5523 10 21 9.55228 21 9V4C21 2.34315 19.6569 1 18 1H10ZM9 7H6.41421L9 4.41421V7ZM14 15.5C14 14.1193 15.1193 13 16.5 13C17.8807 13 19 14.1193 19 15.5V16V17H20C21.1046 17 22 17.8954 22 19C22 20.1046 21.1046 21 20 21H13C11.8954 21 11 20.1046 11 19C11 17.8954 11.8954 17 13 17H14V16V15.5ZM16.5 11C14.142 11 12.2076 12.8136 12.0156 15.122C10.2825 15.5606 9 17.1305 9 19C9 21.2091 10.7909 23 13 23H20C22.2091 23 24 21.2091 24 19C24 17.1305 22.7175 15.5606 20.9844 15.122C20.7924 12.8136 18.858 11 16.5 11Z"
+                                            fill
+                                        />{' '}
+                                    </g>
+                                </svg>
+                            </div>
+                            <div className="text">
+                                {selectedFile === null ? <span>Click to Change Medicine image</span> : <span className=" bg-[#0360D9] px-3 rounded-full">You have selected an image</span>}
+                            </div>
+                            <input id="file" type="file" onChange={handleFileChange} />
+                        </label>
+                    </div>
                                         <div>
                                             <button className="btn bg-[#0360D9] mt-5 text-white w-full" type='submit'> Update Profile</button>
                                         </div>
