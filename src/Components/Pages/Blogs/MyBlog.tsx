2eb42df36c4d6e3ded542218229e0064393d1e11
Mouri199 <138799900+Mouri199@users.noreply.github.com>
2024-02-08 12:02:12 +0600

    modal design fixed

--- src/Components/Pages/Blogs/MyBlog.tsx
+++ src/Components/Pages/Blogs/MyBlog.tsx
@@ -16,7 +16,7 @@ const MyBlog = () => {
         Swal.fire({
             title: "Are you sure?",
             text: "You won't be able to revert this!",
-            icon: "warning",
+            icon: "warning", 
             showCancelButton: true,
             confirmButtonColor: "#3085d6",
             cancelButtonColor: "#d33",
