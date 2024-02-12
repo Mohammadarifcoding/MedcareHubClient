dd88e03287461e789ef0580bd4b2d76e27b16b77
Jihad Hasan <mdjihat0021@gmail.com>
2024-02-12 20:14:00 +0600

    fixed and update SideSearch, MedicineItem, Medicines and SideCategory responsiveness and design

--- src/Components/Pages/Medicines/SideCategory/SideCategory.tsx
+++ src/Components/Pages/Medicines/SideCategory/SideCategory.tsx
@@ -28,11 +28,16 @@ const SideCategory = ({ filter, setFilter }) => {
         }));
     };
 
-
     return (
         <div className="flex items-stretch my-4 px-2">
             {!isLoading ? (
-                <select className="cursor-pointer w-full rounded-md border  md:px-1 py-2 text-center text-[#0360D9]" name="sortBy" id="sortBy" value={filter.sortBy} onChange={handleCategoryChange}>
+                <select
+                    className="cursor-pointer w-full rounded-full border border-[#0360D9] focus:outline-none md:px-1 py-2 text-center text-[#0360D9]"
+                    name="sortBy"
+                    id="sortBy"
+                    value={filter.sortBy}
+                    onChange={handleCategoryChange}
+                >
                     <option value="">Sort By Category</option>
                     {Array.from(new Set(medicines?.map((medicine) => medicine.Category))).map((category) => (
                         <option key={category} value={category}>
