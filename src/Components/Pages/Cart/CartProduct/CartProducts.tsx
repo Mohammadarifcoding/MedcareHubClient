2161161726da0ab470147c97eb6c2ac7d8d3d53e
Mouri199 <138799900+Mouri199@users.noreply.github.com>
2024-02-13 19:01:48 +0600

    cart back to shop button dynamic

--- src/Components/Pages/Cart/CartProduct/CartProducts.tsx
+++ src/Components/Pages/Cart/CartProduct/CartProducts.tsx
@@ -27,14 +27,14 @@ const CartProducts = ({ Products }) => {
       </ul>
       <div className="flex justify-between items-center mt-4">
 
-        <Link to='/medicines'>  
-        <button className="justify-center whitespace-nowrap bg-[#0360D9] hover:bg-[#365885] text-white rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50  hover:text-accent-foreground h-10 px-4 py-2 flex items-center space-x-2">
-          <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
-            <path d="m12 19-7-7 7-7" />
-            <path d="M19 12H5" />
-          </svg>
-          <span>Back to shop</span>
-        </button></Link>
+        <Link to='/medicines'>
+          <button className="justify-center whitespace-nowrap bg-[#0360D9] hover:bg-[#365885] text-white rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50  hover:text-accent-foreground h-10 px-4 py-2 flex items-center space-x-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
+              <path d="m12 19-7-7 7-7" />
+              <path d="M19 12H5" />
+            </svg>
+            <span>Back to shop</span>
+          </button></Link>
         <button onClick={DeleteFullCart} className=" h-10 px-4 py-2 bg-red-600 hover:scale-110 scale-100 rounded transition-all duration-200  text-white ">
           Remove all
         </button>
