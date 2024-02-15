e6a7284adf89502b89036d8cdfcb32ad81d7c472
iamapusaha <apusaha392@gmail.com>
2024-02-16 03:34:35 +0600

    update useAdmin

--- src/Components/Forum/Forum.tsx
+++ src/Components/Forum/Forum.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import FNavBar from './FNavBar.tsx';
 import CreatePost from './CreatePost.tsx';
 import PostBox from './PostBox.tsx';
+import UseAdmin from '../../Hook/UseAdmin.tsx';
 
 const Forum = () => {
+    const [isAdmin] = UseAdmin();
+    console.log(isAdmin);
     return (
         <div className='container mx-auto'>
 
