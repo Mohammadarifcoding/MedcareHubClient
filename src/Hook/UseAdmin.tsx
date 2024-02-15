317c43280e25e185fa1815ffc624b5af80261ffb
iamapusaha <apusaha392@gmail.com>
2024-02-16 02:04:38 +0600

    added UseAdmin hook

---
+++ src/Hook/UseAdmin.tsx
@@ -0,0 +1,27 @@
+import { React, useContext } from 'react';
+import { AuthContext } from '../Providers/AuthProvider/AuthProvider';
+import { useQuery } from '@tanstack/react-query';
+import UseAxiosPublic from './UseAxiosPublic';
+
+const UseAdmin = () => {
+    const { user, load } = useContext(AuthContext)
+    console.log(user);
+    const axiosPublic = UseAxiosPublic();
+    const { data: isAdmin, isPending: isAdminLoading, error: adminError } = useQuery({
+        queryKey: [user?.email, 'isAdmin'],
+        enabled: !load,
+        queryFn: async () => {
+            try {
+                const res = await axiosPublic.get(`/user/role/${user?.email}`)
+                // console.log(res.data);
+                return res.data?.admin || null;
+            } catch (err) {
+                console.error(err);
+                throw new Error('Failed to fetch data');
+            }
+        }
+    })
+    return [isAdmin, isAdminLoading, adminError]
+};
+
+export default UseAdmin;
