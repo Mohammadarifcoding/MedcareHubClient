bd01314663062a2f83c5a68d144326344eb4687a
Mouri199 <138799900+Mouri199@users.noreply.github.com>
2024-02-15 13:54:17 +0600

    doctor que accpeted button dynamic done

--- src/Components/Pages/Doctors/DoctorQueRow.tsx
+++ src/Components/Pages/Doctors/DoctorQueRow.tsx
@@ -2,8 +2,8 @@ import { TiTickOutline } from 'react-icons/ti';
 import { MdDelete } from 'react-icons/md';
 import { AiOutlineDelete } from 'react-icons/ai';
 
-const DoctorQueRow = ({ doctor }) => {
-    const { DocName, DocType, Email, gender } = doctor;
+const DoctorQueRow = ({ doctor, handleChangeDoctorStatus }) => {
+    const { DocName, DocType, Email, gender, status } = doctor;
     return (
         <tr className="bg-[#FFFFFF] hover:bg-[#fafafa7e] ">
             <td className="border-t px-6 py-4 text-center ">{DocName}</td>
@@ -12,12 +12,14 @@ const DoctorQueRow = ({ doctor }) => {
             <td className="border-t px-6 py-4 text-center">{gender}</td>
 
             <td className="px-6 py-4 border-t text-center">
-                <button className="text-white btn btn-ghost  hover:bg-[#393E46] bg-[#0360D9] hover:text-red-800">Pending</button>
+                <button className="text-white btn btn-ghost  hover:bg-[#393E46] bg-[#0360D9] hover:text-red-800">
+                    {status}
+                </button>
             </td>
 
             <td className="px-6 py-4 border-t text-center">
                 <div className="text-3xl flex flex-col md:flex-row items-center justify-center gap-6">
-                    <button className="text-blue-700">
+                    <button onClick={() => handleChangeDoctorStatus(doctor, "Accepted")} className="text-blue-700">
                         <TiTickOutline />
                     </button>
                     <button className="text-red-600">
