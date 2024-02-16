81778c5eb4e3041287ba33a7a399070a05c5cda7
Mohammad Arif <programmermdarif@gmail.com>
2024-02-16 20:02:52 +0600

    Solved the issue

--- src/Components/Pages/Doctors/DoctorQue.tsx
+++ src/Components/Pages/Doctors/DoctorQue.tsx
@@ -42,39 +42,6 @@ const DoctorQue = () => {
             <div className="overflow-x-auto">
 
 
-                <table className="min-w-full border  border-[#0360D9] rounded-xl">
-                    <thead className="bg-[#0360D9] text-white">
-                        <tr>
-
-                            <th className="px-6 py-3 text-center"> Name</th>
-                            <th className="px-6 py-3 text-center"> Doctor Type</th>
-                            <th className="px-6 py-3 text-center"> Email</th>
-                            <th className="px-6 py-3 text-center">Gender</th>
-                            <th className="px-6 py-3 text-center">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody className="rounded-xl">
-                        {doctor?.map((user) => <tr key={user?._id}>
-
-                            <td className="border-t px-6 py-4 text-center ">{user?.DocName}</td>
-                            <td className="border-t px-6 py-4 text-center ">{user?.DocType}</td>
-                            <td className="border-t px-6 py-4 text-center">{user?.Email}</td>
-                            <td className="border-t px-6 py-4 text-center">{user?.gender}</td>
-
-
-
-                            <td className="px-6 py-4 border-t text-center">
-                                <button className="text-white btn btn-ghost  hover:bg-[#393E46] bg-[#0360D9] hover:text-red-800">
-                                    Pending
-                                </button>
-
-
-                            </td>
-                        </tr>)}
-
-                        {/* Add more rows with user details */}
-                    </tbody>
-                </table>
 
 
                 <div className="mt-5 ml-3 md:ml-0 md:my-5">
