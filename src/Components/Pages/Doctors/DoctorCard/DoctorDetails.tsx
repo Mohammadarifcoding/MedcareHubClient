1291bb2eda5275a0fff335d4ff0da51560a66414
afsana-jannat <afsanajannat205@gmail.com>
2024-02-16 15:18:11 +0600

    Booking of a doctor Feature complete

--- src/Components/Pages/Doctors/DoctorCard/DoctorDetails.tsx
+++ src/Components/Pages/Doctors/DoctorCard/DoctorDetails.tsx
@@ -1,9 +1,17 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import React from 'react';
+import UseAuth from '../../../../Hook/UseAuth.tsx';
 
+const workdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 const DoctorDetails = () => {
     const [foundData, setFoundData] = useState();
     const { id } = useParams();
+    const { user } = UseAuth();
+    const [appointmentDate, setAppointmentDate] = useState("")
+    const [patientInfo, setpatientInfo] = useState({})
+    const navigate = useNavigate();
+    console.log(user?.email)
     // console.log(id);
     console.log(foundData);
 
@@ -18,6 +26,63 @@ const DoctorDetails = () => {
                 console.log(error.message);
             });
     }, [id]);
+
+    const handleCheckPatient = async () => {
+        const res = await fetch(`http://localhost:5000/getPatient/${user?.email}`)
+        const data = await res.json()
+
+        if (!data.status)
+            return;
+
+        setpatientInfo(data?.data)
+        document.getElementById('my_modal_5').showModal()
+
+    } // Book korle arif bolsena patient and time r data eksathey add korte, kothay add korbo? I mean kon collection ey add korbo eta ask koro
+
+    const handleAppointment = async () => {
+        // Parse the selected appointment date
+        const selectedDate = new Date(appointmentDate);
+
+        // Get the selected day of the week
+        const selectedDay = selectedDate.toLocaleDateString("en-US", { weekday: "long" });
+
+        // Get the selected time
+        const selectedTime = selectedDate.toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit" });
+
+
+        console.log(selectedDay, selectedTime, appointmentDate);
+        // Check if the doctor is available on the selected day and time
+        // const isAvailable = workdays.includes(selectedDay) &&
+        //     selectedTime >= foundData?.startAvail + '.00' &&
+        //     selectedTime <= foundData?.endAvail + '.00';
+
+        // if (isAvailable) {
+        //     alert("Appointment scheduled successfully!");
+        // } else {
+        //     alert("Doctor is not available at the selected date and time.");
+        // }
+        const patientData = {
+            patient: patientInfo?._id,
+            time: selectedTime,
+            weekDay: selectedDay,
+            doctor: foundData?._id
+        }
+
+        try {
+            const res = await fetch("http://localhost:5000/doctor-booking", {
+                method: "POST",
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(patientData)
+            })
+            const data = await res.json();
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    };
 
     return (
         <>
@@ -112,10 +177,10 @@ const DoctorDetails = () => {
                 </div>
 
                 <div className="flex justify-center space-x-4">
-                    <button class=" items-center justify-center whitespace-nowrap rounded-md text-sm font-medium h-10 px-4 py-2 bg-gray-200 text-gray-500">
+                    <button className=" items-center justify-center whitespace-nowrap rounded-md text-sm font-medium h-10 px-4 py-2 bg-gray-200 text-gray-500">
                         {foundData?.service[1]}
                     </button>
-                    <button class=" items-center justify-center whitespace-nowrap rounded-md text-sm font-medium h-10 px-4 py-2 bg-gray-200 text-gray-500">
+                    <button className=" items-center justify-center whitespace-nowrap rounded-md text-sm font-medium h-10 px-4 py-2 bg-gray-200 text-gray-500">
                         {foundData?.service[2]}
                     </button>
                 </div>
@@ -123,11 +188,38 @@ const DoctorDetails = () => {
 
 
                 <div className='lg:mx-[100px]'>
-                    <div>
-                        <h2 className="text-lg font-semibold mb-4">About Me</h2>
-                        <p className="text-sm mb-4">
-                            {foundData?.aboutMe}
-                        </p>
+                    <div className='flex justify-between'>
+                        <div>
+                            <h2 className="text-lg font-semibold mb-4">About Me</h2>
+                            <p className="text-sm mb-4">
+                                {foundData?.aboutMe}
+                            </p>
+                        </div>
+                        <div>
+                            {/* Open the modal using document.getElementById('ID').showModal() method */}
+                            <button className="btn w-28 bg-[#E1EEFF]" onClick={handleCheckPatient}>
+                                Book Now</button>
+                            <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+                                <div className="modal-box">
+                                    <h2 className='font-semibold text-xl mb-4'>Choose your Appointment Time</h2>
+                                    <label htmlFor="description" className="mt-5">Describe your health status</label>
+                                    <textarea name="" id="description" className="w-full min-h-[150px] border-2 border-gray-100 mt-2 pl-2 pt-2" placeholder="Description"></textarea>
+                                    <label htmlFor="date" className='block mb-4'>Appointment Date</label>
+                                    <input type="datetime-local" onChange={(e) => setAppointmentDate(e.target.value)} name="" id="date" className="" />
+                                    <div className="modal-action">
+                                        <form method="dialog" className='flex justify-between w-full'>
+                                            <div>
+                                                <button onClick={handleAppointment} className="btn bg-[#E1EEFF]">Appointment</button>
+                                            </div>
+                                            {/* if there is a button in form, it will close the modal */}
+                                            <div>
+                                                <button className="btn bg-[#E1EEFF]">Back</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </dialog>
+                        </div>
                     </div>
 
                     <div className='lg:flex justify-between'>
--- src/Components/Pages/Doctors/DoctorQue.tsx
+++ src/Components/Pages/Doctors/DoctorQue.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { base_URL } from '../../../utills/BaseURL.ts';
-import { TiTickOutline } from "react-icons/ti";
 const DoctorQue = () => {
     const [doctor, setdoctor] = useState([])
 
@@ -21,7 +20,7 @@ const DoctorQue = () => {
                 <table className="min-w-full border  border-[#0360D9] rounded-xl">
                     <thead className="bg-[#0360D9] text-white">
                         <tr>
-                           
+
                             <th className="px-6 py-3 text-center"> Name</th>
                             <th className="px-6 py-3 text-center"> Doctor Type</th>
                             <th className="px-6 py-3 text-center"> Email</th>
@@ -31,7 +30,7 @@ const DoctorQue = () => {
                     </thead>
                     <tbody className="rounded-xl">
                         {doctor?.map((user) => <tr key={user?._id}>
-                           
+
                             <td className="border-t px-6 py-4 text-center ">{user?.DocName}</td>
                             <td className="border-t px-6 py-4 text-center ">{user?.DocType}</td>
                             <td className="border-t px-6 py-4 text-center">{user?.Email}</td>
@@ -40,7 +39,7 @@ const DoctorQue = () => {
 
 
                             <td className="px-6 py-4 border-t text-center">
-                                <button  className="text-white btn btn-ghost  hover:bg-[#393E46] bg-[#0360D9] hover:text-red-800">
+                                <button className="text-white btn btn-ghost  hover:bg-[#393E46] bg-[#0360D9] hover:text-red-800">
                                     Pending
                                 </button>
 
