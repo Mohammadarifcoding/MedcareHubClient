66f3b23f0fae607ea62bbfd429ab7049f667ca2c
Jihad Hasan <mdjihat0021@gmail.com>
2024-02-22 19:57:03 +0600

    redesigned DisplayComment

--- src/Components/Forum/DisplayComment.tsx
+++ src/Components/Forum/DisplayComment.tsx
@@ -4,10 +4,12 @@ const DisplayComment = ({ commentData }) => {
     const { id, user, userImg, comment } = commentData;
     return (
         <div>
-            <div className='flex items-center gap-2 my-4'>
-                <img className="w-[40px] h-[40px] bg-slate-500 object-cover rounded-lg hover:blur-[2px] duration-500" src={userImg} alt="" />
-                <h1 className='text-2xl font-bold underline'>{user}:</h1>
-                <p className='text-xl'>{comment}</p>
+            <div className="flex items-start gap-2 my-4">
+                <img className="w-[40px] h-[40px] bg-slate-500 object-cover rounded-full hover:blur-[2px] duration-500" src={userImg} alt="" />
+                <div className="rounded-2xl border shadow-sm bg-[#F0F2F5] px-3 py-2">
+                    <h1 className="text-md font-medium text-[#05051C]">{user}</h1>
+                    <p className="text-md text-[#050505]">{comment}</p>
+                </div>
             </div>
         </div>
     );
