c3a3ef7ccbf15667cb67aff712cd0018fe049bb1
Jihad Hasan <mdjihat0021@gmail.com>
2024-02-22 19:17:09 +0600

    redesigned create post

--- src/Components/Forum/PostBox.tsx
+++ src/Components/Forum/PostBox.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import SinglePost from './SinglePost.tsx';
 import UseAxiosPublic from '../../Hook/UseAxiosPublic.tsx';
-import { useQuery } from "@tanstack/react-query";
+import { useQuery } from '@tanstack/react-query';
 import UseAuth from '../../Hook/UseAuth.tsx';
+import { VscFiles } from "react-icons/vsc";
 
 
 const PostBox = () => {
-    const { user } = UseAuth()
+    const { user } = UseAuth();
     const userEmail = user?.email;
     const axiosPublic = UseAxiosPublic();
     const [queryType, setQueryType] = useState({ type: 'category', value: '' });
@@ -20,36 +21,41 @@ const PostBox = () => {
             } else if (queryType.type === 'userPost') {
                 res = await axiosPublic.get(`/api/forum/${queryType.value}`);
             }
-            return res?.data
+            return res?.data;
         }
-    })
+    });
 
     const handleButtonClick = (type, value) => {
         setQueryType({ type, value });
         refetch();
-    }
-
+    };
 
     return (
         <div>
-            <div className="px-11 py-5 mx-auto">
+            <div className="px-11 py-5 mx-auto ">
                 <div className="flex justify-center items-center">
                     <div>
                         <ul className="flex gap-2">
-                            <button onClick={() => handleButtonClick('category', '')} className="border-4 p-2 border-s-violet-200">All Post</button>
-                            <button onClick={() => handleButtonClick('userPost', userEmail)} className="border-4 p-2 border-s-violet-200">My Post</button>
-                            <button onClick={() => handleButtonClick('category', 'dr-post')} className="border-4 p-2 border-s-violet-200">Dr. Post</button>
-                            <button onClick={() => handleButtonClick('category', 'patient-post')} className="border-4 p-2 border-s-violet-200">Patient Post</button>
+                            <button onClick={() => handleButtonClick('category', '')} className=" p-2 border-s-violet-200 rounded-2xl border shadow-sm bg-white flex items-center gap-2">
+                            <VscFiles />  All Post
+                            </button>
+                            <button onClick={() => handleButtonClick('userPost', userEmail)} className="p-2 border-s-violet-200 rounded-2xl border shadow-sm bg-white">
+                                My Post
+                            </button>
+                            <button onClick={() => handleButtonClick('category', 'dr-post')} className="p-2 border-s-violet-200 rounded-2xl border shadow-sm bg-white">
+                                Dr. Post
+                            </button>
+                            <button onClick={() => handleButtonClick('category', 'patient-post')} className="p-2 border-s-violet-200 rounded-2xl border shadow-sm bg-white">
+                                Patient Post
+                            </button>
                         </ul>
                     </div>
-
                 </div>
             </div>
             <div>
-                {
-                    posts?.map(data => <SinglePost key={data._id} data={data} refetch={refetch}></SinglePost>)
-                }
-
+                {posts?.map((data) => (
+                    <SinglePost key={data._id} data={data} refetch={refetch}></SinglePost>
+                ))}
             </div>
         </div>
     );
