66a0a665c2ca1a40b85d93b40f4c1ff7fb9446ed
iamapusaha <apusaha392@gmail.com>
2024-02-25 02:23:11 +0600

    display all post data on dashboard forum page

--- src/Components/Dashboard/Pages/Forum/ForumAllPost.tsx
+++ src/Components/Dashboard/Pages/Forum/ForumAllPost.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import UseAxiosPublic from '../../../../Hook/UseAxiosPublic.tsx';
 
 const ForumAllPost = () => {
+    const axiosPublic = UseAxiosPublic();
+    const { data: postData = [], refetch } = useQuery({
+        queryKey: ['postData'],
+        queryFn: async () => {
+            const res = await axiosPublic.get('/api/forum/all')
+            return res.data
+        }
+    })
     return (
-        <div>
-            <h1>Hello hello</h1>
+        <div className="overflow-x-auto">
+            <table className="table">
+                {/* head */}
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Post Owner</th>
+                        <th>Title</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        postData?.map((data, idx) => <tr key={data._id}>
+                            <th>{idx + 1}</th>
+                            <td>{data.name}</td>
+                            <td>{data.title}</td>
+                            <td>Blue</td>
+                        </tr>)
+                    }
+
+                </tbody>
+            </table>
         </div>
     );
 };
