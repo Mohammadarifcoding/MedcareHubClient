66753c2c869da17cdf02efc79f3b07b596c32558
iamapusaha <apusaha392@gmail.com>
2024-02-25 03:01:03 +0600

    Approved forum post status

--- src/Components/Dashboard/Pages/Forum/ForumAllPost.tsx
+++ src/Components/Dashboard/Pages/Forum/ForumAllPost.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useQuery } from "@tanstack/react-query";
 import UseAxiosPublic from '../../../../Hook/UseAxiosPublic.tsx';
+import { FcApprove } from "react-icons/fc";
+import Swal from 'sweetalert2';
 
 const ForumAllPost = () => {
     const axiosPublic = UseAxiosPublic();
@@ -11,6 +13,23 @@ const ForumAllPost = () => {
             return res.data
         }
     })
+
+    const handleChangeStatus = (id, status) => {
+        axiosPublic.patch(`/forum/update/status/${id}`, { status })
+            .then(res => {
+                if (res.data) {
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: `is now admin`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                    refetch()
+                }
+            })
+
+    }
     return (
         <div className="overflow-x-auto">
             <table className="table">
@@ -21,6 +40,7 @@ const ForumAllPost = () => {
                         <th>Post Owner</th>
                         <th>Title</th>
                         <th>Status</th>
+                        <th>Approved</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -29,7 +49,12 @@ const ForumAllPost = () => {
                             <th>{idx + 1}</th>
                             <td>{data.name}</td>
                             <td>{data.title}</td>
-                            <td>Blue</td>
+                            <td>{data.status}</td>
+                            <td>
+                                <button onClick={() => handleChangeStatus(data._id, 'Approved')}>
+                                    <FcApprove className='text-4xl'></FcApprove>
+                                </button>
+                            </td>
                         </tr>)
                     }
 
