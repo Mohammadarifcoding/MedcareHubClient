324941559ef2a8012d37a00ac88a05373c12fcf9
iamapusaha <apusaha392@gmail.com>
2024-02-25 02:00:57 +0600

    added dashboard forum route

---
+++ src/Components/Dashboard/Pages/Forum/ForumAllPost.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ForumAllPost = () => {
+    return (
+        <div>
+            <h1>Hello hello</h1>
+        </div>
+    );
+};
+
+export default ForumAllPost;
